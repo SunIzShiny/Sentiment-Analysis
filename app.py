d3ab50eddeb6975ae1971d5d9d6c5723913f21ba
sunny jha <129589862+SunIzShiny@users.noreply.github.com>
2025-02-02 20:41:01 +0545

    final commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+model_filename = 'F:\\6thSem\\Nlp_SentimenAnalysis\\model.sav'
+vectorizer_filename = 'F:\\6thSem\\Nlp_SentimenAnalysis\\vectorizer.sav'
+
+with open(model_filename, 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open(vectorizer_filename, 'rb') as vectorizer_file:
+    vectorizer = pickle.load(vectorizer_file)
+
+st.title('Sentiment Analysis with Logistic Regression')
+
+
+user_input = st.text_area("Enter your text here:")
+
+if st.button('Predict'):
+    if user_input:
+
+        def preprocess_text(content):
+            import re
+            from nltk.corpus import stopwords
+            from nltk.stem.porter import PorterStemmer
+
+            port_stem = PorterStemmer()
+
+            content = re.sub('[^a-zA-Z]', ' ', content)
+            content = content.lower()
+            content = content.split()
+
+            content = [port_stem.stem(word) for word in content if not word in stopwords.words('english')]
+            return ' '.join(content)
+
+
+        preprocessed_input = preprocess_text(user_input)
+
+
+        transformed_input = vectorizer.transform([preprocessed_input])
+
+
+        prediction = model.predict(transformed_input)
+
+
+        if prediction == 1:
+            st.write("Sentiment: Positive")
+        else:
+            st.write("Sentiment: Negative")
+    else:
+        st.write("Please enter some text.")
